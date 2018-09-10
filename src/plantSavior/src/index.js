09d0005684c9bfecb3210e2797a3456b94601532
Sarabjeet Singh <developer.up90@gmail.com>
2018-09-10 13:33:56 +0530

    pessenger pickup game

--- src/plantSavior/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './view/App.js';
-import Tournament from './simulation/tournament';
-
-ReactDOM.render(<Tournament></Tournament>, document.getElementById('simulation'));
-ReactDOM.render(<App></App>, document.getElementById('root'));
